68dd54b6fb43b25d34e50f44d6afe85be8c51f6d
Dain Lee <feb.dain@gmail.com>
2023-02-04 21:24:54 +0900

    refactor: make it more readable

--- level_0/20_remove_vowels.js
+++ level_0/20_remove_vowels.js
@@ -2,7 +2,7 @@
 
 // 문제 설명
 // 영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다.
-// 문자열 my_string이 매개변수로 주어질 때 
+// 문자열 my_string이 매개변수로 주어질 때
 // 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
 
 // 제한사항
@@ -14,8 +14,11 @@
 // "bus"                  "bs"
 // "nice to meet you"     "nc t mt y"
 
-function solution(my_string) { 
-    return my_string.split("").filter(letter => !["a", "i", "e", "o", "u"].includes(letter)).join("");
+function solution(my_string) {
+  return my_string
+    .split("")
+    .filter((letter) => !["a", "i", "e", "o", "u"].includes(letter))
+    .join("");
 }
 
 // 정규 표현식
